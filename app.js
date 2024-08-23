25755ab0beeda3bf24a6339e7ffe88f0b03751e8
Hiroki Kato <hirokikato1@gmail.com>
2024-08-23 18:13:57 -0400

    fetch browser demo

--- app.js
+++ app.js
@@ -6,17 +6,38 @@ const app = express()
 const port = 4000
 
 //this is a callback
-app.get('/wisdom', (req, res) => {
+app.get('/wisdom', async (req, res) => {
     try {
 
+        const response = await fetch('https://api.kanye.rest/')
+        const data = await response.json()
 
+        res.json(data)
 
     } catch (error) {
         console.error('There was a problem fetching kanye:', error)
         res.status(500).json({ error: 'It did not work' });
     }
 
-    res.send('We are on the wisdom page!')
+
+    //fetch this endpoint
+    // fetch('https://api.kanye.rest/')
+    //     .then((response) => {
+    //         if(!response.ok){
+    //             throw new Error('this did not work');
+    //         }
+    //         return response
+    //     })
+    //     .then((data) => {
+    //         return data.json()
+    //     })
+    //     .then((quoteObj) =>{
+    //         return quoteObj
+    //     })
+    //     .then((quote) =>{
+    //         console.log(quote["quote"])
+    //     })
+
 })
 
 app.listen(port, () => {
